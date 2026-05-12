6f00332b73856f39e4bedfdc97373fe14ce9babd
AvanteBrandy2026 <matthew@breakfreebeverages.com>
2026-05-12 09:34:37 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://xsuvahsnaulfopdgxqpp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdXZhaHNuYXVsZm9wZGd4cXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTg0MDIsImV4cCI6MjA5NDA3NDQwMn0.XbTqvGJaOa8UQWuzJfXj0vlF82u9U_q0-YUIybiCQbI'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
